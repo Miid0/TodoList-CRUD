f12258cfd14399b8e4888809bba5f1b1ca27d96d
Elmehdi Ounejjar <elounejj@e1r5p15.1337.ma>
2022-05-25 14:34:05 +0100

    fixing UI and make it responsive

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -8,12 +8,12 @@ interface SearchProps {
 
 export default function SearchBar({ SubmitHandler, value, ChangeHandler }: SearchProps) {
 	return (
-		<form onSubmit={SubmitHandler} className="my-8 w-full flex justify-center">
+		<form onSubmit={SubmitHandler} className="my-8 w-full md:w-[80%] flex justify-center">
 			<input type="text" placeholder='tasks' value={value} onChange={ChangeHandler} 
-				className=" bg-transparent placeholder:text-gray-500 placeholder:italic py-1 px-1.5 outline-none w-[50%]
-					focus:border-b-2 focus:border-zinc-600 focus:rounded-sm focus:border-opacity-100 transition-all duration-100" />
+				className=" bg-transparent placeholder:text-slate-500 placeholder:italic py-1 px-1.5 outline-none w-[50%]
+					focus:border-b-2 focus:border-slate-700 focus:border-opacity-100 transition-all duration-100" />
 			<button type='submit' onClick={SubmitHandler} 
-				className=" mx-2 rounded-md bg-zinc-600 hover:bg-zinc-700 transition-all ease-in-out duration-200 text-gray-100 py-1 px-5 outline-none">Add</button>
+				className=" mx-2 rounded-md bg-slate-700 hover:bg-slate-800 transition-all ease-in-out duration-100 text-gray-100 py-1 px-5 outline-none">Add</button>
 		</form>
 	)
 }
